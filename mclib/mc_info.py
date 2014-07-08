95cb5d3b39b1b73e47135961ab06738c01ab90fd
Mathew Odden <locke105@gmail.com>
2014-07-07 19:08:35 -0500

    Refactor client socket code
    Support different message protocols with the
    same socket connection boilerplate functionality.
    Also, fixes long time issue with End of Stream errors.

--- mclib/mc_info.py
+++ mclib/mc_info.py
@@ -19,6 +19,7 @@
 
 import logging
 import socket
+from cStringIO import StringIO
 from struct import pack
 import traceback
 
@@ -27,86 +28,71 @@ LOG = logging.getLogger(__name__)
 
 
 def get_info(host='localhost', port=25565):
-    trial_and_error = ['MC16', 'MC15']
-
-    for version in trial_and_error:
-        klass = globals()[version]
-        obj = klass()
-        try:
-            return obj.get_info(host, port)
-        except Exception:
-            LOG.warn("Failed to get_info with version %s" % version)
-            LOG.info(traceback.format_exc())
-
-    raise Exception("Couldn't get_info!")
+    return MCServer(host=host, port=port).get_info()
 
 
-class MC16(object):
-    # Strings in the minecraft protocol are encoded as big-endian UCS-2,
-    # prefixed with a short giving its length in characters.
-    @staticmethod
-    def pack_string(string):
-        return pack('>h', len(string)) + string.encode('utf-16be')
+# Strings in the minecraft protocol are encoded as big-endian UCS-2,
+# prefixed with a short giving its length in characters.
+def pack_string(string):
+    return pack('>h', len(string)) + string.encode('utf-16be')
 
-    def get_info(self, host='localhost', port=25565):
-        # Set up our socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(10) # seconds
-        s.connect((host, port))
 
-        # Send 0xFE: Server list ping
-        s.send('\xfe\x01')
+# shared socket connect functionality
+def _get_info(host, port, protocol):
+    # Set up our socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(20) # seconds
+    s.connect((host, port))
 
-        # Send 0xFA: Plugin message
-        s.send('\xfa')                           # ident
-        s.send(self.pack_string('MC|PingHost'))  # message identifier
-        s.send(pack('>h', 7 + 2*len(host)))      # payload length
-        s.send(pack('b', 78))                    # protocol version
-        s.send(self.pack_string(host))           # hostname
-        s.send(pack('>i', port))                 # port
+    s.sendall(protocol._get_message(host, port))
 
-        # Done sending! Read some data and close the socket
-        d = s.recv(1024)
+    data = StringIO()
+    got_bytes = True
+    while got_bytes:
+        in_bytes = s.recv(1024)
+        if len(in_bytes) > 0:
+            data.write(in_bytes)
+        else:
+            got_bytes = False
 
-        # consume extra bytes to avoid server end of stream error
-        s.recv(1024)
-        s.close()
+    s.shutdown(socket.SHUT_RDWR)
+    s.close()
 
-        # Check we've got a 0xFF Disconnect
-        assert d[0] == '\xff'
+    info = protocol.parse_resp(data.getvalue())
+    return info
 
-        # Remove the packet ident (0xFF) and the
-        # short containing the length of the string
-        # Decode UCS-2 string
-        d = d[3:].decode('utf-16be')
 
-        # Check the first 3 characters of the string are what we expect
-        assert d[:3] == u'\xa7\x31\x00'
+class MCServer(object):
 
-        # Split
-        d = d[3:].split('\x00')
+    def __init__(self, host='localhost', port=25565, protocols=None):
+        self.host = host
+        self.port = port
+        if protocols is None:
+            self.protocols = ['MC16', 'MC15']
+        else:
+            self.protocols = protocols
 
-        # Return a dict of values
-        return {'protocol_version': int(d[0]),
-                'server_version':       d[1],
-                'motd':                 d[2],
-                'players':          int(d[3]),
-                'max_players':      int(d[4])}
+    def get_info(self):
+        for protocol in self.protocols:
+            klass = globals()[protocol]
+            protocol_cls = klass()
+            try:
+                return _get_info(self.host, self.port, protocol_cls)
+            except Exception:
+                LOG.warn("Failed to get_info with protocol %s" % protocol)
+                LOG.info(traceback.format_exc())
 
 
 class MC15(object):
-    def get_info(self, host='localhost', port=25565):
-        # Set up our socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host, port))
 
+    def _get_message(self, host, port):
         # Send 0xFE: Server list ping
-        s.send('\xfe\x01')
+        return '\xfe\x01'
 
-        # Read some data
-        d = s.recv(1024)
-        s.close()
+    def get_info(self, host='localhost', port=25565):
+        return _get_info(host, port, self)
 
+    def parse_resp(self, d):
         # Check we've got a 0xFF Disconnect
         assert d[0] == '\xff'
 
@@ -127,3 +113,22 @@ class MC15(object):
                 'motd':                 d[2],
                 'players':          int(d[3]),
                 'max_players':      int(d[4])}
+
+
+class MC16(MC15):
+
+    def _get_message(self, host, port):
+        msg = StringIO()
+
+        # Send 0xFE: Server list ping
+        msg.write('\xfe\x01')
+
+        # Send 0xFA: Plugin message
+        msg.write('\xfa')                           # ident
+        msg.write(pack_string('MC|PingHost'))       # message identifier
+        msg.write(pack('>h', 7 + 2*len(host)))      # payload length
+        msg.write(pack('b', 78))                    # protocol version
+        msg.write(pack_string(host))                # hostname
+        msg.write(pack('>i', port))                 # port
+
+        return msg.getvalue()
